81eeaaee170e089ed992844a592698c1f522ea98
chdonovan <chrisdonovan311@gmail.com>
2021-08-13 17:46:00 -0500

    login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,6 +17,34 @@ const resolvers = {
         }
     },
 
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            console.log(user)
+            const token = signToken(user);
+
+            return {token, user};
+            },
+
+            login: async (parent, { email, password }) => {
+                const user = await User.findOne({ email });
+    
+                if (!user) {
+                    throw new AuthenticationError('Incorrect credentials');
+                }
+    
+                const correctPw = await user.isCorrectPassword(password);
+    
+                if (!correctPw) {
+                    throw new AuthenticationError('Incorrect credentials');
+                }
+    
+                const token = signToken(user);
+                return { token, user };
+            },
+        }
+    }
+
     
 
     
