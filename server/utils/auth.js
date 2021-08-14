04dc7dc04dabc79d795548ac082bfc92167b5b67
chdonovan <chrisdonovan311@gmail.com>
2021-08-14 13:11:25 -0500

    auth=

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,18 +6,22 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query?.token || req.headers?.authorization;
 
     // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+    if (req.headers?.authorization) {
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req
     }
+
 
     // verify token and get user data out of it
     try {
